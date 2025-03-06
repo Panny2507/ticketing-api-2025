63960ecbc694e2c2bb62de10706446826c9d2202
Panny2507 <sajbananna2005@gmail.com>
2025-03-06 18:54:47 +0000

    0306tanf2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from 'nestjs-prisma';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BoardsModule } from './boards/boards.module';
+import { TicketsModule } from './tickets/tickets.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TicketsModule,
+    PrismaModule.forRoot({ isGlobal: true }),
+    BoardsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/dotn.ts
+++ src/dotn.ts
@@ -1,5 +1,4 @@
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -7,14 +6,6 @@ async function bootstrap() {
 
   const port = process.env.PORT ?? 3000;
   console.log(`Nestjs is running on port ${port}`);
-
-  const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setVersion('1.0')
-    .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
-
   await app.listen(port);
 }
 
